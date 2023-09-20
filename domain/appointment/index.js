83169cc692ab60530d62e092f1a7f2662e06059d
VSAETHER <vidar_sather@hotmail.com>
2023-09-20 14:07:35 +0200

    sec test

--- domain/appointment/index.js
+++ domain/appointment/index.js
@@ -30,19 +30,26 @@ class AppointmentDomain {
     return { reference: `#${reference}`, ...result };
   }
   create(data) {
-console.log(data);
     const noSS = data.numero;
-    const nname = data.nom + " " + data.prenom;
+    const nom = data.nom + " " + data.prenom;
     const email = data.mail;
     const number = data.tel;
 
     // Vérifiez que les données sont présentes
-    if (!data || !nname || !email || !noSS || !number) {
+    if (!data || !nom || !email || !noSS || !number) {
       return null;
-
     }
-
-
+    if (
+      !/^[A-Za-zÀ-ÿ\s-]{2,20}$/.test(data.nom) ||
+      !/^[A-Za-zÀ-ÿ\s-]{2,20}$/.test(data.prenom) ||
+      !/^[12][0-9]{2}(0[1-9]|1[0-2])(2[AB]|[0-9]{2})[0-9]{3}[0-9]{3}$/.test(
+        noSS
+      ) ||
+      !/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/.test(email) ||
+      !/^[0-9]{10}$/.test(number)
+    ) {
+      return null;
+    }
     this.storage.create(data);
     return data;
   }
