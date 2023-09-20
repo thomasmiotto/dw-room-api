318d3dadf838c6d46341d7cb83104713c4c829c2
Gauthier.Cl <gauthier.cla@free.fr>
2023-09-20 10:16:31 +0200

    method post

--- domain/appointment/index.js
+++ domain/appointment/index.js
@@ -1,31 +1,50 @@
 class AppointmentDomain {
-    constructor(storage) {
-        this.storage = storage;
+  constructor(storage) {
+    this.storage = storage;
+  }
+
+  list() {
+    const results = this.storage.getAll();
+    const waitingRooms = {
+      A: [],
+      B: [],
+      C: [],
+      D: [],
+    };
+    for (let reference in results) {
+      waitingRooms[reference.charAt(1)].push({
+        reference,
+        ...results[reference],
+      });
+    }
+    return waitingRooms;
+  }
+
+  get(reference) {
+    const result = this.storage.get(`#${reference}`);
+    return { reference: `#${reference}`, ...result };
+  }
+
+  update(reference) {
+    const result = this.storage.get(`#${reference}`);
+    return { reference: `#${reference}`, ...result };
+  }
+  create(data) {
+console.log(data);
+    const noSS = data.numero;
+    const nname = data.nom + " " + data.prenom;
+    const email = data.mail;
+    const number = data.tel;
+
+    // Vérifiez que les données sont présentes
+    if (!data || !nname || !email || !noSS || !number) {
+      return null;
     }
 
-    list() {
-        const results = this.storage.getAll();
-        const waitingRooms = {
-            A: [],
-            B: [],
-            C: [],
-            D: [],
-        };
-        for(let reference in results) {
-            waitingRooms[reference.charAt(1)].push({reference, ...results[reference]});
-        }
-        return waitingRooms;
-    }
 
-    get(reference) {
-        const result = this.storage.get(`#${reference}`);
-        return {reference: `#${reference}`, ...result};
-    }
-
-    update(reference) {
-        const result = this.storage.get(`#${reference}`);
-        return {reference: `#${reference}`, ...result};
-    }
+    this.storage.create(data);
+    return data;
+  }
 }
 
 module.exports.AppointmentDomain = AppointmentDomain;
--- storage/index.js
+++ storage/index.js
@@ -1,39 +1,67 @@
-const fs = require('fs');
-const readline = require('readline');
+const fs = require("fs");
+const readline = require("readline");
 
 const DATABASE = new Map();
 
 const AppointmentStorage = {
-    getAll : () => {
-        const results = {};
-        DATABASE.forEach((value, key) => {
-            results[key] = {...value};
-        });
-        return results;
-    },
-    get : (id) => {
-        return DATABASE.get(id);
-    },
-    update : (id) => {
-        return DATABASE.get(id);
-    }
-}
+  getAll: () => {
+    const results = {};
+    DATABASE.forEach((value, key) => {
+      results[key] = { ...value };
+    });
+    return results;
+  },
+  get: (id) => {
+    return DATABASE.get(id);
+  },
+  update: (id) => {
+    return DATABASE.get(id);
+  },
+  create: (data) => {
+    // Ajoutez le rendez-vous à la base de données
+    fs.appendFile(
+      "./data.csv",
+      [
+        data.id,
+        data.numero,
+        data.nom + " " + data.prenom,
+        data.mail,
+        data.tel,
+      ].join() + "\r\n",
+      "utf8",
+      (err) => {
+        if (err) {
+          console.error(
+            "Une erreur s'est produite lors de l'ajout de la ligne au fichier CSV :",
+            err
+          );
+        } else {
+          console.log("Ligne ajoutée avec succès au fichier CSV.");
+        }
+      }
+    );
+
+    // Renvoyez l'identifiant du rendez-vous créé
+  },
+};
 
 const initStorage = (storage) => {
-    const stream = fs.createReadStream('./data.csv');
-    const rl = readline.createInterface({ input: stream });
-    const today = new Date();
-    rl.on('line', (row) => {
-        const data = row.split(',');
-        storage.set(data[0], {
-            date: today.toLocaleDateString('fr'),
-            noSS: data[1],
-            name: data[2],
-        });
+  const stream = fs.createReadStream("./data.csv");
+  const rl = readline.createInterface({ input: stream });
+  const today = new Date();
+  rl.on("line", (row) => {
+    const data = row.split(",");
+    storage.set(data[0], {
+      date: today.toLocaleDateString("fr"),
+      noSS: data[1],
+      name: data[2],
     });
-    rl.on('close', () => {
-        console.log(`Initialization successfull with ${storage.size} appointments.`);
-    });
+  });
+  rl.on("close", () => {
+    console.log(
+      `Initialization successfull with ${storage.size} appointments.`
+    );
+  });
 };
 
 initStorage(DATABASE);
